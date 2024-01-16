ff7d49caf1c169c4579e99c7039c314c876534e1
Michael James <mikeyj777@gmail.com>
2024-01-16 15:41:33 -0500

    88 - take 1

--- 88_merge_sorted_array.cpp
+++ 88_merge_sorted_array.cpp
@@ -5,27 +5,67 @@
 using namespace std;
 
 class Solution {
+
+public:
+    void print(vector<int>& arr) {
+        for (const int& num : arr)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         // cout << "you are in the merge routine" << " ";
 
-        for (int i = 0; i < m ; i++ ) {
-            
+        if (n == 0) {
+            return;
+        }
+        
+        if (m == 0){
+            nums1 = nums2;
+            return;
         }
 
-        // cout << endl;
+        int pos = nums1.size() - 1;
+        while (true) {
+            if (nums1[m-1] >= nums2[n-1]) {
+                nums1[pos] = nums1[m-1];
+                m--;
+                if (m < 0) {
+                    break;
+                }
+            } else {
+                nums1[pos] = nums2[n-1];
+                n--;
+                if (n < 0) {
+                    break;
+                }
+            }
+            pos--;
+            print(nums1);
+            print(nums2);
+        }
+        cout << endl;
     }
     
 };
 
 int main()
 {
-    vector<int> nums1 = {1,2,3,0,0,0};
-    int m = 3;
-    vector<int> nums2 = {2,5,6};
-    int n = 3;
+    vector<int> nums1 = {1};
+    int m = 1;
+    vector<int> nums2 = {};
+    int n = nums2.size();
+    
     Solution sol = Solution();
     sol.merge(nums1, m, nums2, n);
+    cout << "final" << endl;
+    sol.print(nums1);
+    sol.print(nums2);
+    cout << endl;
+
 }
 
 
