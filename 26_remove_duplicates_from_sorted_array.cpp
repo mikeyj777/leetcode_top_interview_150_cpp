9d5822fd8da3de3f1dbe24b54bf2d87eecf7a0c4
Michael James <mikeyj777@gmail.com>
2024-01-19 10:46:34 -0500

    working

---
+++ 26_remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "helpers.cpp"
+
+using namespace std;
+
+class Solution {
+
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        int j = 1;
+        while (true){
+            mj_printer(nums);
+            if (i >= nums.size()) break;
+            j = i + 1;
+            if (j >= nums.size()) break;
+            while (nums[j] == nums[i]) {
+                nums.erase(nums.begin() + j);
+                if (j >= nums.size()) break;
+            }
+            i++;
+        }
+        cout << "unique values: ";
+        mj_printer(nums);
+        return nums.size();
+    }
+    
+};
+
+int main()
+{
+    Solution sol = Solution();
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
+    int k = sol.removeDuplicates(nums);
+    cout << "count of unique values:  " << k;
+    cout << endl;
+}
+
+
